407d03ffd02349a7564d136ccb3c302d30797c51
Asif Nassar <klmktu.asif@gmail.com>
2025-11-05 20:19:46 +0530

    1.6: Implement type

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -14,6 +15,7 @@ var _ = os.Stdout
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	for {
+		commands := []string{"echo", "exit", "type"}
 		fmt.Fprint(os.Stdout, "$ ")
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
@@ -25,6 +27,14 @@ func main() {
 			os.Exit(0)
 		} else if args[0] == "echo" {
 			fmt.Println(strings.Join(args[1:], " "))
+		} else if args[0] == "type" {
+			if len(args) < 2 {
+				fmt.Println("Usage: type <command>")
+			} else if slices.Contains(commands, args[1]) {
+				fmt.Println(args[1] + " is a shell builtin")
+			} else {
+				fmt.Println(args[1] + ": not found")
+			}
 		} else {
 			fmt.Println(args[0] + ": command not found")
 		}
